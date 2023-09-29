232afe013ce33a4be4c1d66342b808b46e622b77
Archita Pandey <archita.pandey@codemonk.in>
2023-09-29 15:33:47 +0530

    web-crawler

---
+++ src/DatabaseConnection.java
@@ -0,0 +1,31 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    static Connection connection=null;
+
+    public static Connection getConnection() {
+        if (connection != null) {
+            return connection;
+        }
+        String user = "root";
+        String password="Pass@word1";
+        String db= "searchengineapp";
+        return getConnection(user,password,db);
+
+    }
+    private static Connection getConnection(String user, String password, String db){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/" + db + "?user=" + user + "&password=" + password);
+        }
+        catch(SQLException | ClassNotFoundException sqlException){
+            sqlException.printStackTrace();
+        }
+        return  connection;
+
+
+    }
+}
